d94cf6e62ec37b5e81530f8990f10773f69bc62f
sideshowbarker <mike@w3.org>
2026-04-21 19:42:48 +0900

    UI/Qt+UI/AppKit: Add platform AT support for the Qt and AppKit ports/UIs
    This change wires up our Qt and AppKit backends to expose/serve our
    accessibility tree to screen readers and other platform AT.

--- UI/Qt/Tab.cpp
+++ UI/Qt/Tab.cpp
@@ -269,6 +269,8 @@ Tab::Tab(BrowserWindow* window, RefPtr<WebView::WebContentClient> parent_client,
         set_loading(true);
 
         m_location_edit->set_url(url);
+
+        view().schedule_accessibility_tree_request();
     };
 
     view().on_load_finish = [this](auto const&) {
@@ -281,6 +283,7 @@ Tab::Tab(BrowserWindow* window, RefPtr<WebView::WebContentClient> parent_client,
 
     view().on_url_change = [this](auto const& url) {
         m_location_edit->set_url(url);
+        view().schedule_accessibility_tree_request();
     };
 
     QObject::connect(m_location_edit, &QLineEdit::returnPressed, this, &Tab::location_edit_return_pressed);
@@ -288,6 +291,7 @@ Tab::Tab(BrowserWindow* window, RefPtr<WebView::WebContentClient> parent_client,
     view().on_title_change = [this](auto const& title) {
         m_title = qstring_from_utf16_string(title);
         update_tab_title();
+        view().schedule_accessibility_tree_request();
     };
 
     view().on_favicon_change = [this](auto const& bitmap) {
